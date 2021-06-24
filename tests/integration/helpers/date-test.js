4ede8d1cbab04cc850d8a26f08fec2f46cc1d42c
jswob <jacswob@gmail.com>
2021-06-24 22:26:02 +0200

    test: provide test for date helper

--- tests/integration/helpers/date-test.js
+++ tests/integration/helpers/date-test.js
@@ -3,15 +3,18 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Helper | date', function(hooks) {
+module('Integration | Helper | date', function (hooks) {
   setupRenderingTest(hooks);
 
-  // TODO: Replace this with your real tests.
-  test('it renders', async function(assert) {
-    this.set('inputValue', '1234');
+  test('it serializes date correctly', async function (assert) {
+    this.someDate = new Date(1995, 1, 2);
 
-    await render(hbs`{{date inputValue}}`);
+    await render(hbs`{{date someDate}}`);
 
-    assert.equal(this.element.textContent.trim(), '1234');
+    assert.equal(this.element.textContent.trim(), '02-02-1995');
+
+    this.set('someDate', new Date(2011, 11, 12));
+
+    assert.equal(this.element.textContent.trim(), '12-12-2011');
   });
 });
