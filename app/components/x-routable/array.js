ffc8f91a7c2c55207212fc861fddee087348ff80
jswob <jacswob@gmail.com>
2021-06-24 22:14:13 +0200

    chore: add dynamic array

---
+++ app/components/x-routable/array.js
@@ -0,0 +1,16 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { A } from '@ember/array';
+
+export default class XRoutableArrayComponent extends Component {
+  @tracked
+  names = ['Tom', 'Ned', 'Red'];
+
+  @action
+  popLastItem() {
+    this.names.pop();
+
+    this.names = this.names;
+  }
+}
